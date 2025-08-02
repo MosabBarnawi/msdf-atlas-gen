fc1fdd56c19fe4888c8d50d70f8f366c54d7ab39
MSI\justt <just.t17@gmail.com>
2025-08-02 23:07:46 +0300

    Updated Premake file

--- premake5.lua
+++ premake5.lua
@@ -3,7 +3,7 @@ include "msdfgen"
 project "msdf-atlas-gen"
 	kind "StaticLib"
 	language "C++"
-	cppdialect "C++17"
+	cppdialect "C++20"
     staticruntime "off"
 
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
